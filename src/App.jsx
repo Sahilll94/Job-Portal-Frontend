4159a61084c6fd913fd2883f187dbd0271a17793
Sahil <sahilk64555@gmail.com>
2024-09-09 01:21:30 +0530

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Shared/Navbar";
-import Login from "./components/auth/login.jsx";
+import Login from "./components/auth/Login.jsx";
 import Signup from "./components/auth/Signup.jsx";
 import Home from "./components/Home.jsx";
-import Jobs from "./components/Jobs";
+import Jobs from "./components/Jobs"; 
 import Browse from "./components/Browse";
 import Profile from "./components/Profile";
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Shared/Navbar'
-import Herosection from './Herosection'
+import Herosection from './HeroSection'
 import CategoryCarousel from './CategoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './Shared/Footer'
